86c0f6cd1299df06f3885bb37f8b87b56409e824
kira509 <tacetygre87@gmail.com>
2025-07-10 12:54:52 +0300

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+  watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+  isAdmin: { type: Boolean, default: false }
+})
+
+export default mongoose.model("User", userSchema)
