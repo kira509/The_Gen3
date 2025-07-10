d1e5a0ab899aac9deab6f2ba6a25bae55a3d52d0
kira509 <tacetygre87@gmail.com>
2025-07-10 13:16:09 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-black min-h-screen text-white font-sans">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
